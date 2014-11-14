f7de9b51b446bca5f4c62eba0e77127bd6cd9c97
dubpub <dubpubz@gmail.com>
2014-11-15 03:55:41 +0400

    CommodeController added; others removed

---
+++ src/LaravelCommode/Common/Controllers/CommodeController.php
@@ -0,0 +1,62 @@
+<?php
+    namespace LaravelCommode\Common\Controllers;
+
+    use Illuminate\Routing\Controller;
+    use LaravelCommode\Common\Resolver\Resolver;
+
+    /**
+     * Created by PhpStorm.
+     * User: madman
+     * Date: 11/15/14
+     * Time: 3:46 AM
+     */
+    class CommodeController 
+    {
+        protected $resolveMethods = true;
+
+        protected $separateRequests = false;
+        protected $allowAjax = true;
+
+        private function checkParametersResolving($method, $params = array())
+        {
+            if (!$this->resolveMethods) {
+                return $params;
+            }
+
+            $resolver = new Resolver(app());
+            return $resolver->resolveMethodParameters($this, $method, $params);
+        }
+
+        private function checkAllowAjax($isAjax = null)
+        {
+            $isAjax = is_null($isAjax) ? app('request')->ajax() : $isAjax;
+            return $isAjax == !$this->allowAjax;
+        }
+
+        private function checkAjaxMethod($method, $isAjax = null)
+        {
+            $isAjax = is_null($isAjax) ? app('request')->ajax() : $isAjax;
+
+            if ($this->separateRequests && $isAjax)
+            {
+                return 'ajax'.$method;
+            }
+
+            return $method;
+        }
+
+        public function callAction($method, $params = array())
+        {
+            $isAjax = app('request')->ajax();
+
+            if ($this->checkAllowAjax($isAjax))
+            {
+                return $this->missingMethod();
+            }
+
+            $method = $this->checkAjaxMethod($method, $isAjax);
+            $params = $this->checkParametersResolving($method, $params);
+
+            return parent::callAction($method, $params);
+        }
+    }
