f0c59b15605d5c1c0df686157bcd614e57d4daf1
dubpub <dubpubz@gmail.com>
2014-11-20 01:27:01 +0400

    Resolver can force unallowed scope calls; phpdoc added

--- src/LaravelCommode/Common/Resolver/Resolver.php
+++ src/LaravelCommode/Common/Resolver/Resolver.php
@@ -8,25 +8,42 @@
 
     /**
      * Class Resolver
+     *
+     * Resolver is a helper class that can resolve
+     * closures and class methods or turn them into
+     * resolvable closures.
+     *
+     * @author Volynov Andrey
      * @package LaravelCommode\Common\Resolver
      */
     class Resolver
     {
         /**
+         * Laravel application instance. Is used as an
+         * access to laravel's application IoC
+         *
          * @var \Illuminate\Foundation\Application
          */
         private $laraApp;
 
+        /**
+         * Class __constructor
+         * @param Application $laraApp  Laravel application instance.
+         * If it's value is null, it will be initialized from app() function
+         */
         public function __construct(Application $laraApp = null)
         {
-            if (!is_null($laraApp)) {
-                $this->laraApp = $laraApp;
-            } else {
-                $this->laraApp = app();
-            }
+            /**
+             * Initialize laravel IoC
+             */
+            $this->laraApp = !is_null($laraApp) ? $laraApp : app();
         }
 
         /**
+         * Determines if passed ReflectionParameter $parameter can be resolved from IoC.
+         * If $parameter can be resolved and it's bound in IoC it's bound class/interface name
+         * will be returned.
+         *
          * @param ReflectionParameter $parameter
          * @param $reflectionKey
          * @param array $params
@@ -34,11 +51,24 @@
          */
         protected function isResolvable(ReflectionParameter $parameter, $reflectionKey, $params = array())
         {
+            /**
+             * check if parameter available from $params with $reflectionKey key
+             */
             if (isset($params[$reflectionKey])) return false;
 
+            /**
+             * check if $parameter has type
+             */
             if (preg_match('/\[\s\<\w+?>\s([\w\\\\]+)/s', $parameter->__toString(), $matches))
             {
+                /**
+                 * check if $parameter type is not array and can be treated as class/interface
+                 */
                 $canBeCreated = isset($matches[1]) && !$parameter->isArray() && is_string($matches[1]);
+
+                /**
+                 * check if $parameter class/interface exists or it's been bound
+                 */
                 $existsOrBound = class_exists($matches[1]) || $this->laraApp->bound($matches[1]);
 
                 return ($canBeCreated && $existsOrBound) ? $matches[1] : false;
@@ -48,8 +78,10 @@
         }
 
         /**
-         * @param ReflectionParameter[] $reflectionParams
-         * @param array $params
+         * Returns array of values, resolved from ReflectionParameter[] $reflectionParams
+         *
+         * @param ReflectionParameter[] $reflectionParams array of ReflectionParameter
+         * @param array $params array of parameters with known value
          * @return array
          */
         public function resolve($reflectionParams, $params = [])
@@ -65,25 +97,62 @@
             return $params;
         }
 
-        public function method($class, $method, $params = [])
+        /**
+         * Resolves class method parameters and returns them.
+         *
+         * @param string|object $class class instance or class name which method needs to be resolved
+         * @param string $method name of method that needs to be resolved
+         * @param array $params Array of default/known values.
+         * @return mixed returns result of execution resolved $class::$method
+         * @return array
+         */
+        public function resolveMethodParameters($class, $method, $params = [])
         {
-            $reflection = new ReflectionMethod($class, $method);
-            $resolved = $this->resolve($reflection->getParameters(), $params);
+            return $this->resolve((new ReflectionMethod($class, $method))->getParameters(), $params);
+        }
+
+        /**
+         * Resolves class method and calls it.
+         *
+         * @param string|object $class class instance or class name which method needs to be resolved.
+         * @param string $method name of method that needs to be resolved.
+         * @param array $params Array of default/known values.
+         * @param bool $forceScopeCalls suppresses scope exceptions and calls method even if it's
+         * protected or private.
+         * @throws \Exception
+         * @return mixed returns result of execution resolved $class::$method.
+         */
+        public function method($class, $method, $params = [], $forceScopeCalls = false)
+        {
+            $reflectionClass = new \ReflectionClass(is_string($class) ? $this->laraApp[$class] : $class);
+
+            $reflectionMethod = $reflectionClass->getMethod($method);
+
+            $resolved = $this->resolve($reflectionMethod->getParameters(), $params);
 
             try {
                 $result = call_user_func_array([$class, $method], $resolved);
-            } catch(\Exception $e) {
-                if (is_string($class)) {
-                    $class = app($class);
-                    $result = call_user_func_array([$class, $method], $resolved);
+            } catch(\Exception $exception) {
+                if ($forceScopeCalls && ($reflectionMethod->isPrivate() || $reflectionMethod->isProtected())) {
+                    $reflectionMethod->setAccessible(true);
+                    $result = $reflectionMethod->invokeArgs($class, $resolved);
+                    $reflectionMethod->setAccessible(false);
                 } else {
-                    throw $e;
+                    throw $exception;
                 }
+
             }
 
             return $result;
         }
 
+        /**
+         * Returns wrapped into resolvable closure that would call and resolve class method.
+         *
+         * @param string|object $class class instance or class name which method needs to be resolved
+         * @param string $method name of method that needs to be resolved
+         * @return Closure
+         */
         public function methodToClosure($class, $method)
         {
             return function () use ($class, $method)
@@ -92,6 +161,13 @@
             };
         }
 
+        /**
+         * Resolves $closure and executes it
+         *
+         * @param Closure $closure Closure that needs to be resolved.
+         * @param array $params Array of default/known values.
+         * @return mixed Result of $closure execution
+         */
         public function closure(Closure $closure, $params = [])
         {
             $reflection = new \ReflectionFunction($closure);
@@ -99,16 +175,17 @@
             return call_user_func_array($closure, $resolved);
         }
 
+        /**
+         * Wraps make closure resolvable on execution.
+         *
+         * @param Closure $closure Closure that needs to be resolved.
+         * @return Closure
+         */
         public function makeClosure(Closure $closure)
         {
             return function () use ($closure)
             {
                 return $this->closure($closure, func_get_args());
             };
-        }
-
-        public function resolveMethodParameters($class, $method, $params = [])
-        {
-            return $this->resolve((new ReflectionMethod($class, $method))->getParameters(), $params);
         }
     }
